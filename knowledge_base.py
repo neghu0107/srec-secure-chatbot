1880fd89d2d5dec5c7b708a04212476f84deb5de
neha <neha.2401142@srec.ac.in>
2026-02-22 19:40:17 +0530

    Delete knowledge_base.py

--- knowledge_base.py
+++
@@ -1,9 +0,0 @@
-def load_data():
-    with open("srec_data.txt", "r", encoding="utf-8") as f:
-        text = f.read()
-
-    docs = text.split("\n")
-    docs = [d for d in docs if d.strip() != ""]
-    return docs
-
-documents = load_data()
